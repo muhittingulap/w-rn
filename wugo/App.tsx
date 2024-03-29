69bd205e161a7b4f580e0422155b6672e03efd69
Muhittin GÜLAP <gulapmuhittin@gmail.com>
2024-03-30 02:19:25 +0300

    Base application created

---
+++ wugo/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text
+} from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <Text>Wugo Maps App</Text>
+    </SafeAreaView>
+  )
+
+}
+
+export default App;
